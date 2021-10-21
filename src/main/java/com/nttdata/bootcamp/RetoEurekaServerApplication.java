581ec95cb81a93e352067422bb15cff86c04db44
Juanma L <juanmalubian@gmail.com>
2021-10-21 14:39:11 +0200

    Commit inicial

---
+++ src/main/java/com/nttdata/bootcamp/RetoEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RetoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoEurekaServerApplication.class, args);
+	}
+
+}
